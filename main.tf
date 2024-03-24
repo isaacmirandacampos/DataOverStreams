c98038c28b9c9e575dc864ff0383f1b5e4f5adce
Isaac de Miranda Campos <isaacdmcampos@gmail.com>
2024-03-24 13:45:56 -0300

    Remove unused permissions of lambda

--- main.tf
+++ main.tf
@@ -112,12 +112,7 @@ resource "aws_iam_policy" "lambda_dynamodb_access" {
     Version = "2012-10-17",
     Statement = [{
       Action = [
-        "dynamodb:GetItem",
         "dynamodb:PutItem",
-        "dynamodb:Scan",
-        "dynamodb:Query",
-        "dynamodb:UpdateItem",
-        "dynamodb:DeleteItem"
       ],
       Effect   = "Allow",
       Resource = aws_dynamodb_table.dynamodb_table.arn,
